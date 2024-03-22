dc4dcaa3f594d1411508e5b012a663b65a14a31e
Emma <kilokoemma175@gmail.com>
2024-03-22 12:57:00 -0700

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
 # Using Puppet, install flask from pip3.
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  environment => ['LC_ALL=en_US.UTF-8', 'LANG=en_US.UTF-8'],
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
