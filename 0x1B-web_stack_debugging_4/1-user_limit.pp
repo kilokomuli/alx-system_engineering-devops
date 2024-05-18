c710cea0fd4da6f3a27553b52699ab296d42872e
Emma <kilokoemma175@gmail.com>
2024-05-18 12:54:43 -0700

    task1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+#change the OS configuration 
+#and be able to to open a file without any error message
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
